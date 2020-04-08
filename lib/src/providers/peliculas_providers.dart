2ab1ed7042c511236bf6be4c832732f837a237a8
andresperezfc <felipe3apc@gmail.com>
2020-04-08 11:40:59 -0500

    111 - optimizaciones para nuestra aplicación

--- lib/src/providers/peliculas_providers.dart
+++ lib/src/providers/peliculas_providers.dart
@@ -9,6 +9,7 @@ class PeliculasProvider {
   String _language = 'es-ES';
 
   int _popularesPage = 0;
+  bool _cargando = false;
 
   List<Pelicula> _populares = new List();
   final _popularesStreamController =
@@ -40,6 +41,9 @@ class PeliculasProvider {
   }
 
   Future<List<Pelicula>> getPopular() async {
+    if (_cargando) return [];
+
+    _cargando = true;
     _popularesPage++;
     final url = Uri.http(_url, '3/movie/popular', {
       'api_key': _apikey,
@@ -52,6 +56,7 @@ class PeliculasProvider {
     _populares.addAll(resp);
     popularesSink(_populares);
 
+    _cargando = false;
     return resp;
   }
 }
