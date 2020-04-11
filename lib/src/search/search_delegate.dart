d7047e3030868fc341a1d65af5f8b601b8112b77
andresperezfc <felipe3apc@gmail.com>
2020-04-11 16:55:53 -0500

    122 - Buildsuggestion y BuildResults

--- lib/src/search/search_delegate.dart
+++ lib/src/search/search_delegate.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
 
 class DataSearch extends SearchDelegate {
+  String seleccion = '';
+
+  final peliculas = [
+    'spiderman',
+    'superman',
+    'wonder',
+    'loky',
+    'yugioh',
+  ];
+
+  final peliculasRecientes = [
+    'masterclass',
+    'boomter',
+  ];
+
   @override
   List<Widget> buildActions(BuildContext context) {
     // Son las acciones de nuestro appbar
@@ -31,12 +46,40 @@ class DataSearch extends SearchDelegate {
   @override
   Widget buildResults(BuildContext context) {
     // es la instruccion que crea los resultados que vamos a mostrar
-    return Container();
+    return Center(
+      child: Container(
+        height: 100.0,
+        width: 100.0,
+        color: Colors.blueAccent,
+        child: Text(seleccion),
+      ),
+    );
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
     // son las sugerencias que aparecen cuando la persona escribe
-    return Container();
+
+    //aqui se muestra la listadugerida filtrada, si lo que escribe el cliente esta vacio entonces en la lista completa de
+    //peliculas hace la consulta
+    final listaSugerida = (query.isEmpty)
+        ? peliculasRecientes
+        : peliculas
+            .where((p) => p.toLowerCase().startsWith(query.toLowerCase()))
+            .toList();
+
+    return ListView.builder(
+      itemCount: listaSugerida.length,
+      itemBuilder: (context, i) {
+        return ListTile(
+          leading: Icon(Icons.movie),
+          title: Text(listaSugerida[i]),
+          onTap: () {
+            seleccion = listaSugerida[i];
+            showResults(context);
+          },
+        );
+      },
+    );
   }
 }
